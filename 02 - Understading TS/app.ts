59fc203d963bdba137211030a40c4496d926b50a
Jaidev Bangar <solaxes@gmail.com>
2024-05-24 23:20:32 +0530

    Function type with parameters and return type

--- 02 - Understading TS/app.ts
+++ 02 - Understading TS/app.ts
@@ -8,6 +8,11 @@ function printResult(num: number): void {
   console.log("Result : " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 // here we are defining type of the combineValues and we are making it like
@@ -17,3 +22,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
