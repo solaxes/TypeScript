2eca93822713f5f72f51a0174209b3151122cbb1
Jaidev Bangar <solaxes@gmail.com>
2024-05-26 16:24:03 +0530

    Typescript and Modern JavaScript

---
+++ 04 - TypeScript-Modern-JavaScript/dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+const userName = "jaidev";
+let age = 44;
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+const printOutput = (output) => {
+    console.log(output);
+};
+printOutput(add(2, 3));
+const hobbies = ["cricket", "football"];
+const [hobby1, hobby2] = hobbies;
+const person = { firstName: "Jaidev", lastName: "Bangar" };
+const { firstName, lastName } = person;
+//# sourceMappingURL=app.js.map
