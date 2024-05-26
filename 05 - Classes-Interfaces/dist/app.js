0a99eabe57c2c724d93b794a573a924b2b457dcd
Jaidev Bangar <solaxes@gmail.com>
2024-05-26 17:25:39 +0530

    Classes and interfaces

---
+++ 05 - Classes-Interfaces/dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var Department = (function () {
+  function Department(n) {
+    this.name = n;
+  }
+  Department.prototype.describe = function () {
+    console.log("Department: " + this.name);
+  };
+  return Department;
+})();
+var accounting = new Department("Accounting");
+accounting.describe();
+//# sourceMappingURL=app.js.map
