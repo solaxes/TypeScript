8e8999fd1a0e7dc4bce673c9c1140a74fe833171
Jaidev Bangar <solaxes@gmail.com>
2024-05-27 11:47:04 +0530

    Classes and interfaces

--- 05 - Classes-Interfaces/dist/app.js
+++ 05 - Classes-Interfaces/dist/app.js
@@ -1,13 +1,23 @@
 "use strict";
 var Department = (function () {
-  function Department(n) {
-    this.name = n;
-  }
-  Department.prototype.describe = function () {
-    console.log("Department: " + this.name);
-  };
-  return Department;
-})();
+    function Department(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
 var accounting = new Department("Accounting");
+accounting.addEmployee("Jaidev");
+accounting.addEmployee("Lovely");
 accounting.describe();
 //# sourceMappingURL=app.js.map
--- 05 - Classes-Interfaces/src/app.ts
+++ 05 - Classes-Interfaces/src/app.ts
@@ -1,15 +1,35 @@
 class Department {
+  private id: number;
   name: string;
 
-  constructor(n: string) {
+  private employees: string[] = []; // access modifier
+
+  constructor(id: number, n: string) {
     this.name = n;
+    this.id = id;
   }
 
-  describe() {
+  describe(this: Department) {
     console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department(1, "Accounting");
+
+accounting.addEmployee("Jaidev");
+accounting.addEmployee("Lovely");
 
 accounting.describe();
+
+// const accountingCopy = { name: "s", describe: accounting.describe };
+
+// accountingCopy.describe();
